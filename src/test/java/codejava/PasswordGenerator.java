d8555b189730cf5d49ead8201ea374c4207d148d
AmaniAnane <55703723+AmaniAnane@users.noreply.github.com>
2020-07-13 12:07:58 +0100

    update

--- src/test/java/codejava/PasswordGenerator.java
+++ src/test/java/codejava/PasswordGenerator.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordGenerator {
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder =new BCryptPasswordEncoder();
-		String rawPassword="Polly";
+		String rawPassword="Salah";
 		String encodedPassword=encoder.encode(rawPassword);
 		System.out.println(encodedPassword);
 		
